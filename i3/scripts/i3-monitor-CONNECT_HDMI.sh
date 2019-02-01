6ec03405a6b3d513fcad5378cc5f83565d96f0a0
Xaheen <mr.robot.me@yandex.com>
2019-02-01 15:12:54 +0100

    run the script if HRMI is not connection properly

---
+++ i3/scripts/i3-monitor-CONNECT_HDMI.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS-1-1 --mode 1366x768 --pos 0x384 --rotate normal --output DP-1-1 --off --output VGA-1-1 --off --output HDMI-1-1 --mode 2048x1152 --pos 1366x0 --rotate normal
