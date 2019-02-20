4cbaec7fc30245d5b166a977462a487a32ef8f60
Xaheen <mr.robot.me@yandex.com>
2019-02-20 23:57:12 +0100

    Minor fixes

--- others/clonings.sh
+++ others/clonings.sh
@@ -5,3 +5,5 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
