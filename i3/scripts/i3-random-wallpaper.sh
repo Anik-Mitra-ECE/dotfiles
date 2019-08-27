9f9622a4bad316fff2ad47a026505a9ea5a0fc00
Xaheen <mr.robot.me@yandex.com>
2019-08-27 19:40:47 +0200

    changed path

--- i3/scripts/i3-random-wallpaper.sh
+++ i3/scripts/i3-random-wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-  nitrogen --set-zoom-fill --no-recurse --random ~/Dropbox/Xaheen_public/wallpaper_collections/i3_wallaper
+  nitrogen --set-zoom-fill --no-recurse --random ~/Dropbox/PUBLIC/wallpaper_collections/i3_wallaper
   sleep 5m
 done
